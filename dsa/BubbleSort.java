dbbbc32902c169011ff5b66c99b8a1e50dde0694
Vishal <mgvishalctr@gmail.com>
2026-03-10 23:37:29 +0530

    push

---
+++ dsa/BubbleSort.java
@@ -0,0 +1,52 @@
+public class BubbleSort {
+
+    // Method to perform bubble sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < n - 1 - i; j++) {
+
+                // Compare adjacent elements
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // If no swapping happened, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Method to print array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+}
