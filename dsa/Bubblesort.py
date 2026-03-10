3d58710f70607f084aabb8bdce7cdc03e778995f
Vishal <mgvishalctr@gmail.com>
2026-03-10 23:35:27 +0530

    commit

---
+++ dsa/Bubblesort.py
@@ -0,0 +1,24 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        swapped = False
+
+        for j in range(n - 1 - i):
+            if arr[j] > arr[j + 1]:
+                # swap elements
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+
+        # stop if already sorted
+        if not swapped:
+            break
+
+# Example
+arr = [5, 1, 4, 2, 8]
+
+print("Original array:", arr)
+
+bubble_sort(arr)
+
+print("Sorted array:", arr)
