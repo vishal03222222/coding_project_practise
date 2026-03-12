f76d6733e898708393a59e44f4168c3b6eb859e5
Vishal <mgvishalctr@gmail.com>
2026-03-12 23:29:47 +0530

    commit

---
+++ dsa/Sorting/Insertionsort.py
@@ -0,0 +1,13 @@
+arr = [12, 11, 13, 5, 6]
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+print("Sorted array:", arr)
